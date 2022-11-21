5c4485850ecde512d2c9c48762df1ce837dc32d2
wzr0728 <dtjfhjtf@gmail.com>
2022-11-21 16:29:35 +0800

    version3

--- controller_buyer.py
+++ controller_buyer.py
@@ -6,3 +6,18 @@ def getActiveList():
     cur.execute(sql)
     records = cur.fetchall()
     return records
+def getDetail(id):
+    sql = "select id, UiD,name, deadline from SaleCase where id = %s;"
+    sql2 = "select OiD, UiD, price from OrderCase where OiD = %s;"
+    cur.execute(sql)
+    SalesRecords = cur.fetchall()
+    cur.execute(sql2)
+    OrderRecords = cur.fetchall()
+    return SalesRecords, OrderRecords # 標案資訊, 標案下標價格
+def bid(id, price, UiD):
+    if(id == None or price == None):
+        return False
+    sql = "insert into OrderCase (OiD, UiD, price) values (%s,%s,%s);"
+    cur.execute(sql,(OiD, UiD, price))
+    conn.commit()
+    return True
